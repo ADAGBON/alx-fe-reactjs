40a56f2169431f2213e68dca24c1a6b0575bbdcc
Law Adagbon <nadagbon@gmail.com>
2026-03-07 17:55:44 +0100

    Add favorites and recommendations features

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,8 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 import { useParams } from 'react-router-dom';
 
 const RecipeDetailsWrapper = () => {
@@ -21,6 +23,8 @@ function App() {
               <SearchBar />
               <AddRecipeForm />
               <RecipeList />
+              <FavoritesList />
+              <RecommendationsList />
             </div>
           } />
           <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
